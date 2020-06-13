2e6845044a04bbcf65735234bbc3347adad33b76
vicc91 <vlroa30@gmail.com>
2020-06-12 20:58:16 -0500

    Add handler error http request

--- src/app/home/components/home-card/home-card.component.ts
+++ src/app/home/components/home-card/home-card.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Album } from '../../../shared/models/album';
 
 @Component({
   selector: 'sp-home-card',
--- src/app/shared/services/spotify.service.ts
+++ src/app/shared/services/spotify.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 import { environment } from '../../../environments/environment';
-import { Observable } from 'rxjs';
 import { Album } from '../models/album';
 import { Track } from '../models/track';
 import { Artist } from '../models/artist';
@@ -11,34 +12,49 @@ import { Artist } from '../models/artist';
   providedIn: 'root'
 })
 export class SpotifyService {
+
+  constructor(private http: HttpClient) { }
   private newReleasesUrl = `${environment.baseUrl}/album/new-releases?country=CO`;
   private albumUrl = `${environment.baseUrl}/album`;
   private searchUrl = `${environment.baseUrl}/search?`;
   private artistUrl = `${environment.baseUrl}/artists`;
 
-  constructor(private http: HttpClient) { }
+  private static handleError(err: any) {
+    if (err.error instanceof ErrorEvent) {
+      // A client side or network error occurred
+      return throwError(`A ocurrido un error ${err.error.message}`);
+    }
+    // The backend returned an unsuccessful response code
+    return throwError(`Servicio web retorna código ${err.error.httpStatusCode}: ${err.error.message}`);
+  }
 
   getNewReleases(): Observable<Album[]> {
-    return this.http.get<Album[]>(this.newReleasesUrl);
+    return this.http.get<Album[]>(this.newReleasesUrl)
+      .pipe(catchError(SpotifyService.handleError));
   }
 
   getTracksAlbum(id: string): Observable<Track[]> {
-    return this.http.get<Track[]>(`${this.albumUrl}/${id}/tracks`);
+    return this.http.get<Track[]>(`${this.albumUrl}/${id}/tracks`)
+      .pipe(catchError(SpotifyService.handleError));
   }
 
   getAlbumById(id: string): Observable<Album> {
-    return this.http.get<Album>(`${this.albumUrl}/${id}`);
+    return this.http.get<Album>(`${this.albumUrl}/${id}`)
+      .pipe(catchError(SpotifyService.handleError));
   }
 
   searchArtists(searchValue: string): Observable<Artist[]> {
-    return this.http.get<Artist[]>(`${this.searchUrl}q=${searchValue}`);
+    return this.http.get<Artist[]>(`${this.searchUrl}q=${searchValue}`)
+      .pipe(catchError(SpotifyService.handleError));
   }
 
   getArtistById(id: string): Observable<Artist> {
-    return this.http.get<Artist>(`${this.artistUrl}/${id}`);
+    return this.http.get<Artist>(`${this.artistUrl}/${id}`)
+      .pipe(catchError(SpotifyService.handleError));
   }
 
   getTopTracksByArtist(id: string): Observable<Track[]> {
-    return this.http.get<Track[]>(`${this.artistUrl}/${id}/top-tracks`);
+    return this.http.get<Track[]>(`${this.artistUrl}/${id}/top-tracks`)
+      .pipe(catchError(SpotifyService.handleError));
   }
 }
