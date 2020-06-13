f07300601e4e833fd7bc68013de02ff43da7c3f5
vicc91 <vlroa30@gmail.com>
2020-06-12 21:30:54 -0500

    Add enviroment variables for deploy

--- src/app/shared/services/spotify.service.ts
+++ src/app/shared/services/spotify.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 import { environment } from '../../../environments/environment';
